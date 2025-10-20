b0b192c1479b765695db970fd831b4e596875118
ozguryedibela <oyedibela21@gmail.com>
2025-10-20 21:57:18 +0300

    Create Task-2.cpp

---
+++ Task-2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+void areaCalculating () {
+	
+	double width, height;
+	cout << "Enter the width of the rectangle: ";
+	cin >> width;
+	cout << "Enter the height of the rectangle: ";
+	cin >> height;
+
+	double area = width * height / 2;
+	ofstream file("rectangle_area.txt");
+
+	if (file.is_open()) {
+	
+		cout << "File created.";
+		file << area;
+		file.close();
+
+	}
+}
+
+int main() {
+	
+	areaCalculating();
+
+}
